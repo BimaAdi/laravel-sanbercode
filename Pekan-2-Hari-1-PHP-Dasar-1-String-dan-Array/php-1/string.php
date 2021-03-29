2910b400d0d09dd086cfad6676c76032a17e7466
BimaAdi <bimaadi419@gmail.com>
2021-03-29 20:02:12 +0700

    Menyelesaikan soal php dasar string dan array

--- Pekan-2-Hari-1-PHP-Dasar-1-String-dan-Array/php-1/string.php
+++ Pekan-2-Hari-1-PHP-Dasar-1-String-dan-Array/php-1/string.php
@@ -23,6 +23,11 @@
 
         $first_sentence = "Hello PHP!" ; // Panjang string 10, jumlah kata: 2
         $second_sentence = "I'm ready for the challenges"; // Panjang string: 28,  jumlah kata: 5
+
+        echo "Panjang string: ".strlen($first_sentence)."<br>";
+        echo "Jumlah kata: ".str_word_count($first_sentence)."<br>";
+        echo "Panjang string: ".strlen($second_sentence)."<br>";
+        echo "Jumlah kata: ".str_word_count($second_sentence)."<br>";
         
         echo "<h3> Soal No 2</h3>";
         /* 
@@ -36,8 +41,8 @@
         echo "<label>String: </label> \"$string2\" <br>";
         echo "Kata pertama: " . substr($string2, 0, 1) . "<br>" ; 
         // Lanjutkan di bawah ini
-        echo "Kata kedua: " ;
-        echo "<br> Kata Ketiga: " ;
+        echo "Kata kedua: ".substr($string2, 2, 4);
+        echo "<br> Kata Ketiga: ".substr($string2, 6, 7);
 
         echo "<h3> Soal No 3 </h3>";
         /*
@@ -47,6 +52,7 @@
         $string3 = "PHP is old but sexy!";
         echo "String: \"$string3\" "; 
         // OUTPUT : "PHP is old but awesome"
+        echo "<br> OUTPUT: \"".str_replace("sexy!", "awesome" ,$string3)."\"";
 
     ?>
 </body>
